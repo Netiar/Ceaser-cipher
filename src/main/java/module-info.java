1539d326d9c9b10256beb7ba2a750510de636957
Raindrop <collins.odinga@student.moringaschool.com>
2022-04-24 16:46:32 +0300

    feat: added Messageciphertest file

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.ceasercipher {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.ceasercipher to javafx.fxml;
-    exports com.example.ceasercipher;
-}
