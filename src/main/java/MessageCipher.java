55f39a63514ed7b03d444f858f668048fee7b7b2
Raindrop <collins.odinga@student.moringaschool.com>
2022-04-25 01:15:53 +0300

    test: testing of MessageCipher word

--- src/main/java/MessageCipher.java
+++ src/main/java/MessageCipher.java
@@ -15,7 +15,7 @@ public class MessageCipher {
         return alphabet[position];
     }
 
-    public String cipherWord(String word){
+    String cipherWord(String word){
         char[] wordCharacterArray= word.toUpperCase().toCharArray();
         StringBuilder cipherWord = new StringBuilder();
 
@@ -26,7 +26,43 @@ public class MessageCipher {
         return cipherWord.toString();
     }
 
+    private char encoding(String plainText, int key) throws Exception {
+        int n = plainText.length();
+
+        if (key < 1 || key > 25) throw new Exception("The key must be between 1 and 25");
+        char cipher = 0;
+        for(int i = 0; i<n; i++){
+            char letter = plainText.charAt(i);
+            if(Character.isLetter(letter)) {
+                if (Character.isUpperCase(letter)) {
+                    char r = (char) (letter + key);
+                    if (r > 'Z') {
+                        cipher += (char) (letter - (26 - key));
+                    } else {
+                        cipher += r;
+                    }
+                } else if (Character.isLowerCase(letter)) {
+                    char r = (char) (letter + key);
+                    if (r > 'z') {
+                        cipher += (char) (letter - (26 - key));
+                    } else {
+                        cipher += r;
+                    }
+                }
+            }else {
+               cipher += letter;
+            }
+        }
+
+        return  cipher;
+
+    }
+
+
+
+
 }
+
 
 
 
--- src/test/java/MessageCipherTest.java
+++ src/test/java/MessageCipherTest.java
@@ -1,26 +1,44 @@
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MessageCipherTest {
     @Test
-    public void cipherLetter_returnsCharPlusTwoPosition_J(){
+    public void cipherLetter_returnsCharPlusTwoPosition_J() {
         MessageCipher messageCipher = new MessageCipher();
         Character expected = 'J';
         assertEquals(expected, messageCipher.cipherLetter('H'));
     }
 
     @Test
-    public void cipherLetter_returnsCharPlusTwoPosition_K(){
+    public void cipherLetter_returnsCharPlusTwoPosition_K() {
         MessageCipher messageCipher = new MessageCipher();
         Character expected = 'K';
         assertEquals(expected, messageCipher.cipherLetter('I'));
     }
 
     @Test
-    public void cipherWord_returnsCipherWord_JK(){
+    public void cipherWord_returnsCipherWord_JK() {
         MessageCipher messageCipher = new MessageCipher();
         String expected = "JK";
         assertEquals(expected, messageCipher.cipherWord("HI"));
+    }
+
+    @Test
+    public void cipherWord_returnsCipherWord_NCBA() {
+        MessageCipher messageCipher = new MessageCipher();
+        String expected = "NCBA";
+        assertEquals(expected, messageCipher.cipherWord("LAZY"));
+    }
+
+    @Test
+    public void cipherPlainText_returnsCiphertext_() {
+       try {
+           String expected = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+           String actual = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+       }
+        catch (Exception ex){
+            assertEquals("The key must be between 1 and 25",ex.getMessage());}
     }
 }
